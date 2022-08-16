ebfeac78dba1a425f2f56df9683853bd74d2e75f
Nastya Filippova <nastya.filippova87@gmail.com>
2022-08-16 12:15:21 +0300

    feat: add class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        return Math.round((this.minNum + this.maxNum) / 2)
     }
 
     lower() {
-
+        return this.maxNum = Math.round((this.minNum + this.maxNum) / 2);
     }
 
     greater() {
-
+        return this.minNum = Math.round((this.minNum + this.maxNum) / 2);
     }
 }
 
